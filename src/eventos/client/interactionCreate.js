2b00e52f3a604f9033ff17205a18b7e93854e019
Xrls-O <finnussy@gmail.com>
2025-01-15 20:05:07 -0500

    Update interactionCreate.js

--- src/eventos/client/interactionCreate.js
+++ src/eventos/client/interactionCreate.js
@@ -1,17 +1,14 @@
-module.exports = {
-    name: 'interactionCreate',
-    async execute(interaction) {
-        if (!interaction.isCommand()) return;
+module.exports = async (client, interaction) => {
+    if (!interaction.isCommand()) return;
 
-        const command = interaction.client.commands.get(interaction.commandName);
+    const command = client.commands.get(interaction.commandName);
 
-        if (!command) return;
+    if (!command) return;
 
-        try {
-            await command.execute(interaction);
-        } catch (error) {
-            console.error(error);
-            await interaction.reply({ content: 'Hubo un error al ejecutar este comando.', ephemeral: true });
-        }
-    },
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'Hubo un error ejecutando este comando!', ephemeral: true });
+    }
 };
