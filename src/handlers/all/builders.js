16a7603237b05ea334519b4f0a3ae15f1d8c690b
Xrls-O <finnussy@gmail.com>
2025-01-15 20:07:01 -0500

    Update builders.js

--- src/handlers/all/builders.js
+++ src/handlers/all/builders.js
@@ -1,32 +1,11 @@
-const { REST, Routes } = require('discord.js');
 const fs = require('fs');
 
 module.exports = (client) => {
-    client.handleCommands = async () => {
-        const commandFiles = fs.readdirSync('./src/slashCommands/generales').filter(file => file.endsWith('.js'));
+    client.commands = new Map();
+    const commandFiles = fs.readdirSync('./src/slashCommands/generales').filter(file => file.endsWith('.js'));
 
-        const commands = [];
-        for (const file of commandFiles) {
-            const command = require(`../../slashCommands/generales/${file}`);
-            commands.push(command.data.toJSON());
-            client.commands.set(command.data.name, command);
-        }
-
-        const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
-
-        (async () => {
-            try {
-                console.log('Recargando comandos de aplicación (/)');
-
-                await rest.put(
-                    Routes.applicationCommands(process.env.CLIENT_ID),
-                    { body: commands },
-                );
-
-                console.log('Comandos recargados exitosamente');
-            } catch (error) {
-                console.error(error);
-            }
-        })();
-    };
+    for (const file of commandFiles) {
+        const command = require(`../slashCommands/generales/${file}`);
+        client.commands.set(command.name, command);
+    }
 };
