9697e17a8e58707a22b60100781b8b40d2502568
Xrls-O <finnussy@gmail.com>
2025-01-15 20:08:24 -0500

    Update eventos.js

--- src/handlers/all/eventos.js
+++ src/handlers/all/eventos.js
@@ -1,18 +1,11 @@
 const fs = require('fs');
 
 module.exports = (client) => {
-    client.handleEvents = async () => {
-        const eventFolders = fs.readdirSync('./src/eventos');
-        for (const folder of eventFolders) {
-            const eventFiles = fs.readdirSync(`./src/eventos/${folder}`).filter(file => file.endsWith('.js'));
-            for (const file of eventFiles) {
-                const event = require(`../../eventos/${folder}/${file}`);
-                if (event.once) {
-                    client.once(event.name, (...args) => event.execute(...args));
-                } else {
-                    client.on(event.name, (...args) => event.execute(...args));
-                }
-            }
-        }
-    };
+    const eventFiles = fs.readdirSync('./src/eventos/client').filter(file => file.endsWith('.js'));
+
+    for (const file of eventFiles) {
+        const eventName = file.split('.')[0];
+        const event = require(`../eventos/client/${file}`);
+        client.on(eventName, event.bind(null, client));
+    }
 };
